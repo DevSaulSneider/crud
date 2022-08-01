5ca7abb89231567baaee3ba47731b50299eed07a
SaulSneider <saulsneiderwork@outlook.com>
2022-07-31 23:35:49 -0500

    se agrego ;

--- test_dni.php
+++ test_dni.php
@@ -1,3 +1,3 @@
 <?php
-echo file_get_contents("https://apiperu.dev/api/dni/71021010?api_token=595e8153b138ec364b1054f897b34b47b27dee4b65e5c8589c778ca3ae3ade7c")
+echo file_get_contents("https://apiperu.dev/api/dni/71021010?api_token=595e8153b138ec364b1054f897b34b47b27dee4b65e5c8589c778ca3ae3ade7c");
 ?>
